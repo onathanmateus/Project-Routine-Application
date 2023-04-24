808b3c9572b6298b470d5bbbfcaa7f605531497b
Nathan Mateus <nathanmateudeo@hotmail.com>
2023-04-24 16:15:44 -0300

    I updated the script.js

--- script.js
+++ script.js
@@ -6,20 +6,20 @@ button.addEventListener("click", add)
 form.addEventListener("change", save)
 
 function add(){
-    const today = new Date().toLocaleDateString("pt-br").slice(0, 5)
-    const dayExists = nlwSetup.dayExists(today)
+  const today = new Date().toLocaleDateString("pt-br").slice(0, 5)
+  const dayExists = nlwSetup.dayExists(today)
 
-    if(dayExists){
-        alert("Dia já incluso ❌")
-        return
-    }
+  if(dayExists){
+    alert("Dia já incluso ❌")
+    return
+  }
 
-    alert("Dia adicionado com sucesso ✅")
-    nlwSetup.addDay(today)
+  alert("Dia adicionado com sucesso ✅")
+  nlwSetup.addDay(today)
 }
 
 function save(){
-    localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
+  localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data))
 }
 
 const data = JSON.parse(localStorage.getItem("NLWSetup@habits"))
